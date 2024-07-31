a3acaa5d02596f6eec20e065782ee91df8b6ebf2
diogomene <diogodelimamenezes15@gmail.com>
2024-07-31 03:33:03 -0400

    feat: adiciona cors em back-end, para permitir acesso por navegador

--- serverSide/index.ts
+++ serverSide/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './api/main.router';
+import cors from 'cors'
 
 const app = express();
 const {PORT = 3000} = process.env;
 
 app.use(express.json());
+app.use(cors());
 app.use(router)
 
 app.listen(PORT, () => {
